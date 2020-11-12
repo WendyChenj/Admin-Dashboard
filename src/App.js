abb03e635526ec898526dfdcbbfbcef397c6dc11
Wenjie Chen <wenjiechen@WenjiedeMacBook-Air.local>
2020-11-12 11:20:05 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import Navbar from './components/Navbar/Navbar';
 import SideDrawer from './components/Drawer/Drawer';
 import Dashboard from './components/Dashboard/Dashboard';
+import BlankPage from './components/BlankPage/BlankPage';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className='app-conent'>
         <SideDrawer />
-        <Dashboard />
+        <Switch>
+          <Route path='/analytics'>
+            <Dashboard />
+          </Route>
+        </Switch>
       </div>
-      
     </div>
+    </Router>
   );
 }
 
